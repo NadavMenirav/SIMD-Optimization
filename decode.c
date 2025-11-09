90eb911636d120765f93826712ac65c3bbb03d2f
Nadav Menirav <nmenirav@gmail.com>
2025-11-09 17:19:04 +0200

    (sdf)

--- decode.c
+++ decode.c
@@ -5,7 +5,7 @@ long decode_c_version(long x, long y, long z) {
     y = y - z;
     x = x * y;
     result = y;
-    result = result % 2 == 0 ? 0: -1;
+    result = result % 2 == 0 ? 0: -1; // The arithmetic shift
     result = result ^ x;
     return result;
 }
