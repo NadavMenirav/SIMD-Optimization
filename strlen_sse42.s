750368173266d8a7ebdb7923ac6191e8c0ac8156
Nadav Menirav <nmenirav@gmail.com>
2025-11-09 18:23:42 +0200

    (Feat) starting the function

--- strlen_sse42.s
+++ strlen_sse42.s
@@ -2,6 +2,17 @@
 .globl strlen_sse42
 
 strlen_sse42:
-    pushq %rcx
+    pushq %rcx #administrations
     xorl %ecx, %ecx
+
+    pxor xmm0, xmm0 # The null terminator string
+    movdqu (%rdi), xmm1 # the first 16 bytes of our string
+
+    xorl %eax, %eax # zero the eax register
+
+loop: # while we did not reach the end of the string
+    pcmpistri xmm0, xmm1, 0x08 # first index that they are equal
+    cmpl 16, %ecx
+    je loop
+    jump outside
 
