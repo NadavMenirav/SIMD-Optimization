9dc034d0c9fbf6d9300f03ffbba5403472e48262
Nadav Menirav <nmenirav@gmail.com>
2025-11-14 14:28:54 +0200

    (Feat) putting the algorithm in the array

--- hamming_distance/hamming_intrinsics.c
+++ hamming_distance/hamming_intrinsics.c
@@ -32,37 +32,38 @@ int hamming_dist(char str1[MAX_STR], char str2[MAX_STR]) {
     const char* pstr1 = str1;
     const char* pstr2 = str2;
 
-    // Storing the strings in the xmm registers
-    const __m128i firstString = _mm_loadu_si128((__m128i *)pstr1);
-    const __m128i secondString = _mm_loadu_si128((__m128i *)pstr2);
-
     for (int i = 0; i < numberOfIterations; i++) {
 
         // Moving the strings up to the next 16 bytes.
         pstr1 += i * 16;
         pstr2 += i * 16;
 
+        // Storing the strings in the xmm registers
+        const __m128i firstString = _mm_loadu_si128((__m128i *)pstr1);
+        const __m128i secondString = _mm_loadu_si128((__m128i *)pstr2);
+
+        /*
+        * The function returns a xmm register that every bit in it represents whether the corresponding chars in
+        * the array are equal
+        */
+        const __m128i matches = _mm_cmpeq_epi8(secondString, firstString);
+
+        // The function sets the register 0
+        const __m128i zeroString = _mm_setzero_si128();
+
+        /*
+        * The function splits the register in half and sums over it. we use the zero string because it first subtracts it
+        * from our string
+        */
+        const __m128i sumOfMatches= _mm_sad_epu8(matches, zeroString);
+
+        // The number of natches is calculated by taking the lower 16 bits  sumOfMatches and the 16 bits in the second half
+        const int numberOfMatches = _mm_extract_epi32(sumOfMatches, 0) + _mm_extract_epi32(sumOfMatches, 4);
+
+        const int numberOfDiff = 16 - numberOfMatches;
 
     }
-    /*
-     * The function returns a xmm register that every bit in it represents whether the corresponding chars in
-     * the array are equal
-     */
-    const __m128i matches = _mm_cmpeq_epi8(secondString, firstString);
 
-    // The function sets the register 0
-    const __m128i zeroString = _mm_setzero_si128();
-
-    /*
-     * The function splits the register in half and sums over it. we use the zero string because it first subtracts it
-     * from our string
-     */
-    const __m128i sumOfMatches= _mm_sad_epu8(matches, zeroString);
-
-    // The number of natches is calculated by taking the lower 16 bits  sumOfMatches and the 16 bits in the second half
-    const int numberOfMatches = _mm_extract_epi32(sumOfMatches, 0) + _mm_extract_epi32(sumOfMatches, 4);
-
-    const int numberOfDiff = 16 - numberOfMatches;
 }
 
 int local_strlen(const char *s) {
